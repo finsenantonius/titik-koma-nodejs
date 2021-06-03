fa920cb2c8d6819ac1c679185bcae74da9e0d785
Finsen <finsenantonius42@gmail.com>
2021-06-03 19:38:04 +0700

    deployment #2

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 //import route
-const authRoute = require('./routes/auth');
-const postRoute = require('./routes/post');
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/post");
 dotenv.config();
 
 //Connect to DB
 // mongoose.connect(process.env.DB_CONNECT,
 // { useNewUrlParser: true, useUnifiedTopology = true },()=>console.log('connected to db'))
 
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true },()=>console.log('connected to db'));
+mongoose.connect(
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("connected to db")
+);
 
 //middlewares
 app.use(express.json());
 //route middleware
-app.use('/api/user/',authRoute);
-app.use('/api/post/',postRoute);
+app.use("/api/user/", authRoute);
+app.use("/api/post/", postRoute);
 
-app.listen(3000,()=>console.log('Server up and running'));
+app.listen(3000, () => console.log("Server up and running"));
